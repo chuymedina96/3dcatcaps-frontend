e3925281d0f23bfb1f0837e2beb939f222c1dc06
Jesus Medina <chuymedina96@gmail.com>
2025-07-17 09:21:05 -0500

    more

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require("express");
-const path = require("path");
-
+const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve the static files from the React build folder
-app.use(express.static(path.join(__dirname, "build")));
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
 
-// Handle SPA routing (serves index.html for any unknown routes)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+// Catch-all route to serve index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
